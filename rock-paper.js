c9bdf89d187546a8e61901a60a412a6ac89740d7
colinsachin06 <sachin0623@icloud.com>
2025-08-14 18:15:42 +0530

    Update rock-paper.js

--- rock-paper.js
+++ rock-paper.js
@@ -54,10 +54,10 @@ let score = JSON.parse(localStorage.getItem('score')) || {
       <img src="${playerMove}.png" class="movebutton">
       <img src="${computerMove}.png" class="movebutton">
       Computer`; */
-             document.querySelector('.jsmove').innerHTML = `You 
+             /* document.querySelector('.jsmove').innerHTML = `You 
   <img src="${playerMove}.png" class="result-move">
   <img src="${computerMove}.png" class="result-move">
-  Computer`;
+  Computer`; */
 
         alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}
  wins: ${score.wins}, losses: ${score.losses}, ties:${score.ties}`);          
